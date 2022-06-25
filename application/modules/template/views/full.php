80a12888b8591f4c15bc654b8d1a48f6d05e7374
Ilpras <ilprasetyo2125@gmail.com>
2022-06-25 17:21:48 +0700

    Update View

--- application/modules/template/views/full.php
+++ application/modules/template/views/full.php
@@ -10,39 +10,41 @@
     <meta name="keywords" content="admin template, Kostlab admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>Sistem Pakar</title>
-    <link rel="apple-touch-icon" href="<?= base_url()?>assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url()?>assets/images/ico/favicon.ico">
+    <link rel="apple-touch-icon" href="<?= base_url() ?>assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url() ?>assets/images/ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendors/css/vendors.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendors/css/charts/apexcharts.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendors/css/extensions/tether-theme-arrows.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendors/css/extensions/tether.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendors/css/extensions/shepherd-theme-default.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendors/css/charts/apexcharts.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendors/css/extensions/tether-theme-arrows.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendors/css/extensions/tether.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendors/css/extensions/shepherd-theme-default.css">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/colors.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/themes/semi-dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/components.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/themes/semi-dark-layout.css">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/core/colors/palette-gradient.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/pages/dashboard-analytics.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/pages/card-analytics.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/plugins/tour/tour.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/core/colors/palette-gradient.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/pages/dashboard-analytics.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/pages/card-analytics.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/plugins/tour/tour.min.css">
     <!-- END: Page CSS-->
-    <?php if(isset($css)){$this->load->view($css);} ?>
+    <?php if (isset($css)) {
+        $this->load->view($css);
+    } ?>
 
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/style.css">
     <!-- END: Custom CSS-->
 
 </head>
@@ -58,13 +60,15 @@
             <ul class="nav navbar-nav flex-row">
                 <li class="nav-item mr-auto"><a class="navbar-brand" href="#">
                         <div class="brand-logo"></div>
-                        <h2 class="brand-text mb-0">KostLab</h2>
+                        <h2 class="brand-text mb-0">Sistem Pakar</h2>
                     </a></li>
                 <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary" data-ticon="icon-disc"></i></a></li>
             </ul>
         </div>
         <div class="shadow-bottom"></div>
-        <?php if(isset($sidebar)){$this->load->view($sidebar);} ?>
+        <?php if (isset($sidebar)) {
+            $this->load->view($sidebar);
+        } ?>
     </div>
 
 
@@ -89,10 +93,10 @@
                                 <!--   a.nav-link.nav-menu-main.menu-toggle.hidden-xs(href='#')-->
                                 <!--     i.ficon.feather.icon-menu-->
                             </ul>
-                          
+
                         </div>
                         <ul class="nav navbar-nav float-right">
-                           
+
                             <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
                                     <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">Admin</span><span class="user-status">Available</span></div><span><img class="round" src="<?= base_url() ?>assets/images/portrait/small/avatar-s-11.png" alt="avatar" height="40" width="40" /></span>
                                 </a>
@@ -115,10 +119,10 @@
                             <h2 class="content-header-title float-left mb-0">Floating Navbar</h2>
                             <div class="breadcrumb-wrapper col-12">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="<?= base_url("admin")?>">Home</a>
+                                    <li class="breadcrumb-item"><a href="<?= base_url("admin") ?>">Home</a>
                                     </li>
-                                    <?php if(isset($titlePage)):?><li class="breadcrumb-item"><a href="#"><?php echo $titlePage ?> </a>
-                                    </li><?php endif?>
+                                    <?php if (isset($titlePage)) : ?><li class="breadcrumb-item"><a href="#"><?php echo $titlePage ?> </a>
+                                        </li><?php endif ?>
                                 </ol>
                             </div>
                         </div>
@@ -126,7 +130,9 @@
                 </div>
             </div>
             <div class="content-body">
-              <?php if(isset($content)){$this->load->view($content);} ?>
+                <?php if (isset($content)) {
+                    $this->load->view($content);
+                } ?>
             </div>
         </div>
     </div>
@@ -145,43 +151,45 @@
 
 
     <!-- BEGIN: Vendor JS-->
-    <script src="<?= base_url()?>assets/vendors/js/vendors.min.js"></script>
-    <script src="<?= base_url()?>assets/vendors/js/forms/select/select2.full.min.js"></script>
+    <script src="<?= base_url() ?>assets/vendors/js/vendors.min.js"></script>
+    <script src="<?= base_url() ?>assets/vendors/js/forms/select/select2.full.min.js"></script>
     <!-- BEGIN: Page Vendor JS-->
-    <script src="<?= base_url()?>assets/vendors/js/charts/apexcharts.min.js"></script>
-    <script src="<?= base_url()?>assets/vendors/js/extensions/tether.min.js"></script>
-    <script src="<?= base_url()?>assets/vendors/js/extensions/shepherd.min.js"></script>
+    <script src="<?= base_url() ?>assets/vendors/js/charts/apexcharts.min.js"></script>
+    <script src="<?= base_url() ?>assets/vendors/js/extensions/tether.min.js"></script>
+    <script src="<?= base_url() ?>assets/vendors/js/extensions/shepherd.min.js"></script>
     <!-- END: Page Vendor JS-->
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
-    <script src="<?= base_url()?>assets/vendors/js/ui/prism.min.js"></script>
+    <script src="<?= base_url() ?>assets/vendors/js/ui/prism.min.js"></script>
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="<?= base_url()?>assets/js/core/app-menu.js"></script>
-    <script src="<?= base_url()?>assets/js/core/app.js"></script>
+    <script src="<?= base_url() ?>assets/js/core/app-menu.js"></script>
+    <script src="<?= base_url() ?>assets/js/core/app.js"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
-    <script src="<?= base_url()?>assets/js/scripts/pages/dashboard.js"></script>  <!-- Uncomment ini untuk start tour-->
-    <?php if(isset($js)){$this->load->view($js);} ?>
+    <script src="<?= base_url() ?>assets/js/scripts/pages/dashboard.js"></script> <!-- Uncomment ini untuk start tour-->
+    <?php if (isset($js)) {
+        $this->load->view($js);
+    } ?>
     <script type="text/javascript">
-    $(function(){
-        var current = window.location.href;
-        $('#sidebar li a').each(function(){
-            var $this = $(this);
-            // if the current path is like this link, make it active
-            if($this.attr('href')== current){
-              // alert($this[1]);
-              if($this.parents('.sub-item').length==1){
-                $this.parents('.sub-item').addClass('active');
-              }else{
-                $this.parents('.nav-item').addClass('active');
-              }
-            }
+        $(function() {
+            var current = window.location.href;
+            $('#sidebar li a').each(function() {
+                var $this = $(this);
+                // if the current path is like this link, make it active
+                if ($this.attr('href') == current) {
+                    // alert($this[1]);
+                    if ($this.parents('.sub-item').length == 1) {
+                        $this.parents('.sub-item').addClass('active');
+                    } else {
+                        $this.parents('.nav-item').addClass('active');
+                    }
+                }
+            })
         })
-    })
     </script>
     <!-- END: Page JS-->
 
